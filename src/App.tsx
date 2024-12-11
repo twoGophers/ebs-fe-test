d4f7bd79a21a702ff68a47a6d1e28d3b7ee930f9
Vladimir <falinvlad93@rambler.ru>
2024-12-11 17:21:02 +0200

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import Catalog from "../components/catalog/Catalog";
+import Cart from "../components/cart/Cart";
+import Icon from "../components/ui/Icon";
 
-// components
-import Catalog from '../components/catalog/Catalog';
-import Cart from '../components/cart/Cart';
-import Product from '../components/product/Product';
-
-function App() {
+export default function App() {
   return (
-    <div style={{ textAlign: 'center' }} className="text-3xl font-bold underline text-amber-300">
-      <h1>Welcome to the Front-End Developer Test !</h1>
-      <p>We wish you the best of luck. Please make sure to read the README file for
-        instructions.</p>
-        
-        <Router>
-        <div>
+    <div className="flex flex-col min-h-screen">
+      <Router>
+        <header className="flex space-x-4 bg-gray-800 text-white">
+          <div className="container p-4 flex flex-row justify-between">
+            <nav >
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `font-medium mr-10 text-xl hover:text-yellow-400 transition  ${isActive ? "text-yellow-400 font-bold" : "text-white "}`
+                }
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/cart"
+                className={({ isActive }) =>
+                  `font-medium text-xl hover:text-yellow-400 transition  ${isActive ? "text-yellow-400 font-bold" : "text-white "}`
+                }
+              >
+                Cart
+              </NavLink>
+            </nav>
+            <div className="flex flex-row h-full items-center ">
+              <span className="w-10 h-10 "> 
+                <Icon name={'cart'} />
+              </span>
+              <span className="text-lg">
+                0
+              </span>
+            </div>
+          </div>
+
+        </header>
+        {/* Основной контент */}
+        <main className="container flex-grow p-4">
           <Routes>
             <Route path="/" element={<Catalog />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/product" element={<Product />} />
           </Routes>
-        </div>
+        </main>
+        {/* Футер */}
+        <footer className="bg-gray-800 text-white p-4 text-center">
+          Footer Content
+        </footer>
       </Router>
     </div>
-  )
+  );
 }
-
-export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Matches your components
+    "./src/**/*.{js,jsx,ts,tsx,scss,css}", // Matches your components
   ],
   theme: {
     extend: {},
