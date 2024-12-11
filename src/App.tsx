eee39944696e3717ea716bd0a84203329fa6b982
Vladimir <falinvlad93@rambler.ru>
2024-12-11 15:36:26 +0200

    add new plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+
+// components
+import Catalog from '../components/catalog/Catalog';
+import Cart from '../components/cart/Cart';
+import Product from '../components/product/Product';
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Welcome to the Front-End Developer Test!</h1>
+    <div style={{ textAlign: 'center' }} className="text-3xl font-bold underline text-amber-300">
+      <h1>Welcome to the Front-End Developer Test !</h1>
       <p>We wish you the best of luck. Please make sure to read the README file for
         instructions.</p>
+        
+        <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Catalog />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/product" element={<Product />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Matches your components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
